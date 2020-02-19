6d5c5a3f0638d50769f152c0b46ca80d5bd783a7
Duncan McGreggor <oubiwann@gmail.com>
2020-02-19 17:20:40 -0600

    Added Status enum.

--- src/component.rs
+++ src/component.rs
@@ -3,3 +3,17 @@ pub struct Component {
     pub name: &'static str,
     pub dependencies: Vec<&'static str>,
 }
+
+pub enum Status {
+    Off,
+    Startng,
+    Running,
+    Restarting,
+    Restarted,
+    Failing,
+    Retrying,
+    Recovering,
+    Failed,
+    Stopping,
+    Terminated,
+}
