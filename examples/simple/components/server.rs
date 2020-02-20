f52053157a131be2035017e61b685b25d63f2b3e
Duncan McGreggor <oubiwann@gmail.com>
2020-02-20 01:57:29 -0600

    Try non-blocking TCP listener.

--- examples/simple/components/server.rs
+++ examples/simple/components/server.rs
@@ -1,6 +1,7 @@
+use std::io;
 use std::io::Write;
-use std::net::TcpListener;
-use std::thread;
+use std::net::{TcpListener, TcpStream};
+use std::{thread, time};
 
 #[derive(Debug)]
 pub struct Server {
@@ -9,7 +10,10 @@ pub struct Server {
 
 impl Server {
     pub fn new() -> Server {
-        let listener = TcpListener::bind("0.0.0.0:1234").unwrap();
+        let listener = TcpListener::bind("0.0.0.0:12345").unwrap();
+        listener
+            .set_nonblocking(true)
+            .expect("Cannot set non-blocking");
         println!("TCP listener set up to run on port 1234 ...");
         Server { listener: listener }
     }
@@ -17,16 +21,35 @@ impl Server {
     pub fn start(&self) {
         println!("Starting server component ...");
         for stream in self.listener.incoming() {
-            thread::spawn(|| {
-                let mut stream = stream.unwrap();
-                stream.write(b"Connected ...\r\n").unwrap();
-            });
+            match stream {
+                Ok(s) => {
+                    self.handle_connection(s);
+                }
+                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                    // Decide if we should exit
+                    println!("Error: {:?}", e);
+                    thread::sleep(time::Duration::from_millis(5000));
+                    break;
+                    // Decide if we should try to accept a connection again
+                    // continue;
+                }
+                Err(e) => panic!("encountered IO error: {}", e),
+            }
         }
     }
 
     pub fn stop(&self) {
-        println!("Stopping server component ...");
+        println!("Stopping server component ...")
         // self.listener.
+    }
+
+    pub fn handle_connection(&self, mut s: TcpStream) {
+        println!("Connected ...");
+        s.write(b"Connected ...\r\n").unwrap();
+        // thread::spawn(|| {
+        //     let mut s = s;
+        //     s.write(b"Connected ...\r\n").unwrap();
+        // });
     }
 }
 
